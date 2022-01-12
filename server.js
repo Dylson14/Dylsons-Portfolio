ea0f3e82a9ceb2b3ff6faa68de90269c1e416ef0
Dylson Oliveira <dylsonuni@gmail.com>
2022-01-12 20:53:39 +0000

    using sendfile to send the index.html file

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-    res.send("Hello World")
+    res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 app.listen(PORT, () => {
